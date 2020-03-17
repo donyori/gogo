f11e13095e8beba8f64d1d96db114c1b443e93c1
Yuan Gao <donyoridoyodoyo@outlook.com>
2020-03-17 19:51:06 +0800

    Add package runtime.

---
+++ runtime/caller.go
@@ -0,0 +1,71 @@
+// gogo. A Golang toolbox.
+// Copyright (C) 2019-2020 Yuan Gao
+//
+// This file is part of gogo.
+//
+// gogo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package runtime
+
+import (
+	stdruntime "runtime"
+	"strings"
+)
+
+// Return the package name of the function fn. fn is the package path-qualified
+// function name that uniquely identifies a single function in the program.
+func FuncPkg(fn string) string {
+	// The first dot ('.') after the last slash ('/') splits the package name
+	// and the function name. The dots in the package name after the last slash
+	// are escaped to URL encoding ('.' -> "%2e").
+	i := strings.LastIndex(fn, "/")
+	i = strings.Index(fn[i+1:], ".") + i + 1
+	return fn[:i]
+}
+
+// Return the stack frame information of caller. skip is the number of
+// stack frames to ascend, with 0 identifying the caller of CallerFrame.
+// The return value ok is false if the information is unretrievable.
+func CallerFrame(skip int) (frame stdruntime.Frame, ok bool) {
+	rpc := make([]uintptr, 1)
+	n := stdruntime.Callers(skip+2, rpc)
+	if n < 1 {
+		return
+	}
+	frame, _ = stdruntime.CallersFrames(rpc).Next()
+	return frame, frame.PC != 0
+}
+
+// Return the full package name and simple function name of
+// the function in the given stack frame.
+// The return value ok is false if the information is unretrievable.
+func FramePkgFunc(frame stdruntime.Frame) (pkg, fn string, ok bool) {
+	if frame.PC == 0 || frame.Function == "" {
+		return
+	}
+	pkg = FuncPkg(frame.Function)
+	return pkg, frame.Function[len(pkg)+1:], true
+}
+
+// Return the full package name and simple function name of caller.
+// skip is the number of stack frames to ascend,
+// with 0 identifying the caller of CallerPkgFunc.
+// The return value ok is false if the information is unretrievable.
+func CallerPkgFunc(skip int) (pkg, fn string, ok bool) {
+	frame, ok := CallerFrame(skip + 1)
+	if !ok {
+		return
+	}
+	return FramePkgFunc(frame)
+}
---
+++ runtime/caller_test.go
@@ -0,0 +1,124 @@
+// gogo. A Golang toolbox.
+// Copyright (C) 2019-2020 Yuan Gao
+//
+// This file is part of gogo.
+//
+// gogo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package runtime
+
+import "testing"
+
+type TestExportedStruct struct{}
+
+func (tes *TestExportedStruct) Foo() (pkg, fn string) {
+	pkg, fn, _ = CallerPkgFunc(0)
+	return pkg, fn
+}
+
+func (tes *TestExportedStruct) foo() (pkg, fn string) {
+	pkg, fn, _ = CallerPkgFunc(0)
+	return pkg, fn
+}
+
+type testLocalStruct struct{}
+
+func (tls *testLocalStruct) Foo() (pkg, fn string) {
+	pkg, fn, _ = CallerPkgFunc(0)
+	return pkg, fn
+}
+
+func (tls *testLocalStruct) foo() (pkg, fn string) {
+	pkg, fn, _ = CallerPkgFunc(0)
+	return pkg, fn
+}
+
+var testGlobalTagPkgFuncs [][3]string
+
+func init() {
+	pkg, fn, _ := CallerPkgFunc(0)
+	testGlobalTagPkgFuncs = append(testGlobalTagPkgFuncs, [3]string{"init-1", pkg, fn})
+}
+
+func init() {
+	pkg, fn, _ := CallerPkgFunc(0)
+	testGlobalTagPkgFuncs = append(testGlobalTagPkgFuncs, [3]string{"init-2", pkg, fn})
+}
+
+func _TestShowCallerPkgFunc(t *testing.T) {
+	var records []struct {
+		tag string
+		pkg string
+		fn  string
+	}
+	for _, elem := range testGlobalTagPkgFuncs {
+		records = append(records, struct {
+			tag string
+			pkg string
+			fn  string
+		}{tag: elem[0], pkg: elem[1], fn: elem[2]})
+	}
+	pkg, fn, _ := CallerPkgFunc(0)
+	records = append(records, struct {
+		tag string
+		pkg string
+		fn  string
+	}{tag: "TestFunc", pkg: pkg, fn: fn})
+	func() {
+		defer func() {
+			pkg, fn, _ := CallerPkgFunc(0)
+			records = append(records, struct {
+				tag string
+				pkg string
+				fn  string
+			}{tag: "inner-2", pkg: pkg, fn: fn})
+		}()
+		pkg, fn, _ := CallerPkgFunc(0)
+		records = append(records, struct {
+			tag string
+			pkg string
+			fn  string
+		}{tag: "inner-1", pkg: pkg, fn: fn})
+	}()
+	tes := new(TestExportedStruct)
+	pkg, fn = tes.Foo()
+	records = append(records, struct {
+		tag string
+		pkg string
+		fn  string
+	}{tag: "TES.Foo", pkg: pkg, fn: fn})
+	pkg, fn = tes.foo()
+	records = append(records, struct {
+		tag string
+		pkg string
+		fn  string
+	}{tag: "TES.foo", pkg: pkg, fn: fn})
+	tls := new(testLocalStruct)
+	pkg, fn = tls.Foo()
+	records = append(records, struct {
+		tag string
+		pkg string
+		fn  string
+	}{tag: "TLS.Foo", pkg: pkg, fn: fn})
+	pkg, fn = tls.foo()
+	records = append(records, struct {
+		tag string
+		pkg string
+		fn  string
+	}{tag: "TLS.foo", pkg: pkg, fn: fn})
+
+	for i := range records {
+		t.Logf("Record %d: %+v.", i, records[i])
+	}
+}
