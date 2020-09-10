5f3e3062162f99889e70b5e2f25e082d56470557
Yuan Gao <donyoridoyodoyo@outlook.com>
2020-09-10 11:34:06 +0800

    Add OnceIndicator.

---
+++ concurrency/once_indicator.go
@@ -0,0 +1,95 @@
+// gogo. A Golang toolbox.
+// Copyright (C) 2019-2020 Yuan Gao
+//
+// This file is part of gogo.
+//
+// gogo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package concurrency
+
+import "sync"
+
+// An object that performs exactly one action, just like sync.Once.
+// Moreover, it can indicate whether the action has been performed or not,
+// and enable the client on another goroutine to wait for the action to finish.
+type OnceIndicator interface {
+	// Perform the same as the method Do of sync.Once, and indicate whether
+	// the function f is called in this invocation.
+	//
+	// In detail, it calls the function f and returns true iff the method Do
+	// is being called for the first time for this instance of OnceIndicator.
+	// Otherwise, it does nothing but waits for the first call of f to finish,
+	// and then returns false.
+	//
+	// In a special case, if the client wants to do nothing but trigger
+	// this indicator, just set f to nil.
+	Do(f func()) bool
+
+	// Return a channel that will be closed after calling the method Do
+	// for the first time for this instance of OnceIndicator.
+	C() <-chan struct{}
+
+	// Wait for the first call of the method Do for this instance
+	// on another goroutine to finish.
+	Wait()
+
+	// Test whether the method Do for this instance is called or not.
+	//
+	// It returns true iff the first call of the method Do
+	// for this instance has finished.
+	Test() bool
+}
+
+// Create a new instance of OnceIndicator.
+func NewOnceIndicator() OnceIndicator {
+	return &onceIndicator{c: make(chan struct{})}
+}
+
+// An implementation of interface OnceIndicator.
+type onceIndicator struct {
+	once sync.Once     // Once object.
+	c    chan struct{} // Channel to broadcast the finish signal.
+}
+
+func (oi *onceIndicator) Do(f func()) bool {
+	r := false
+	oi.once.Do(func() {
+		r = true
+		defer close(oi.c)
+		if f != nil {
+			f()
+		}
+	})
+	return r
+}
+
+func (oi *onceIndicator) C() <-chan struct{} {
+	return oi.c
+}
+
+func (oi *onceIndicator) Wait() {
+	select {
+	case <-oi.c:
+		// Do nothing.
+	}
+}
+
+func (oi *onceIndicator) Test() bool {
+	select {
+	case <-oi.c:
+		return true
+	default:
+		return false
+	}
+}
---
+++ concurrency/once_indicator_test.go
@@ -0,0 +1,93 @@
+// gogo. A Golang toolbox.
+// Copyright (C) 2019-2020 Yuan Gao
+//
+// This file is part of gogo.
+//
+// gogo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package concurrency
+
+import (
+	"sync"
+	"testing"
+)
+
+type testOne int
+
+func (o *testOne) Increase() {
+	*o++
+}
+
+func TestOnceIndicator_Do(t *testing.T) {
+	o := new(testOne)
+	oi := NewOnceIndicator()
+	const N = 10
+	rs := make([]bool, N)
+	var wg sync.WaitGroup
+	wg.Add(N)
+	for i := 0; i < N; i++ {
+		go func(rank int) {
+			defer wg.Done()
+			rs[rank] = oi.Do(func() {
+				o.Increase()
+			})
+			if v := *o; v != 1 {
+				t.Errorf("Once failed: %d != 1.", v)
+			}
+		}(i)
+	}
+	wg.Wait()
+	if *o != 1 {
+		t.Errorf("Once failed: %d != 1.", *o)
+	}
+	cntr := 0
+	for _, r := range rs {
+		if r {
+			cntr++
+		}
+	}
+	if cntr != 1 {
+		t.Errorf("Not only one call of Do return true. #true: %d.", cntr)
+	}
+}
+
+func TestOnceIndicator_Do_Panic(t *testing.T) {
+	oi := NewOnceIndicator()
+	func() {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Fatal("OnceIndicator.Do did NOT panic.")
+			}
+		}()
+		oi.Do(func() {
+			panic("first panic")
+		})
+	}()
+
+	oi.Do(func() {
+		t.Fatal("OnceIndicator.Do called twice.")
+	})
+}
+
+func TestOnceIndicator_Do_NilF(t *testing.T) {
+	oi := NewOnceIndicator()
+	if !oi.Do(nil) {
+		t.Error("OnceIndicator.Do returns false on the first call.")
+	}
+	select {
+	case <-oi.C():
+	default:
+		t.Error("OnceIndicator.Do did NOT trigger the channel after calling Do with f set to nil.")
+	}
+}
