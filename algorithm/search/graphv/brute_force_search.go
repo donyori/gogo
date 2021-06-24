8dbaae207e10e966d4b510e2e5687b1beac5c64f
Yuan Gao <donyoridoyodoyo@outlook.com>
2021-06-25 06:26:50 +0800

    Add package graphv.

---
+++ algorithm/search/graphv/brute_force_search.go
@@ -0,0 +1,485 @@
+// gogo. A Golang toolbox.
+// Copyright (C) 2019-2021 Yuan Gao
+//
+// This file is part of gogo.
+//
+// gogo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package graphv
+
+import "github.com/donyori/gogo/algorithm/search/internal"
+
+// BasicInterface represents a graph used in the graph search algorithm,
+// where only the vertices are concerned.
+//
+// It contains the basic methods required by every graph search algorithm.
+type BasicInterface interface {
+	// Root returns the vertex of the graph where the search algorithms start.
+	//
+	// It returns only one vertex because the search starts at
+	// one specified vertex in most cases.
+	// If there are multiple starting vertices, set the root to a dummy vertex
+	// whose adjacent vertices are the starting vertices.
+	Root() interface{}
+
+	// Adjacency returns the adjacent vertices of the specified vertex
+	// as an adjacency list.
+	//
+	// The first item in the list will be accessed first.
+	Adjacency(vertex interface{}) []interface{}
+
+	// SetGoal sets the search goal.
+	//
+	// It will be called once at the beginning of the search functions.
+	//
+	// Its implementation can do initialization for each search in this method.
+	SetGoal(goal interface{})
+
+	// Access examines the specified vertex.
+	//
+	// It returns an indicator found to reports whether the specified vertex
+	// is the search goal.
+	//
+	// Sometimes it is also referred to as "visit".
+	Access(vertex interface{}) (found bool)
+
+	// Discovered reports whether the specified vertex
+	// has been examined by the method Access.
+	//
+	// Sometimes it is also referred to as "visited".
+	//
+	// It is typically implemented by using a hash table or
+	// associating an attribute "discovered" to each vertex.
+	//
+	// If it is not necessary to record the vertices discovered,
+	// or the graph is too big to record the vertices discovered,
+	// just always return false.
+	// In this case, the search algorithms may access one vertex
+	// multiple times, and may even fall into an infinite loop.
+	Discovered(vertex interface{}) bool
+}
+
+// Dfs finds goal in itf using depth-first search algorithm,
+// and returns the goal vertex found.
+//
+// It returns nil if goal is not found.
+//
+// goal is only used to call the method SetGoal of itf.
+// It's OK to handle goal in your implementation of BasicInterface,
+// and set goal to an arbitrary value, such as nil.
+func Dfs(itf BasicInterface, goal interface{}) interface{} {
+	itf.SetGoal(goal)
+	v := itf.Root()
+	if v == nil {
+		return nil
+	}
+	stack, idx := [][]interface{}{{v}}, 0
+	for idx >= 0 {
+		adj, i := stack[idx], 0
+		// Skip nil and discovered vertices.
+		for i < len(adj) && (adj[i] == nil || itf.Discovered(adj[i])) {
+			i++
+		}
+		if i >= len(adj) {
+			// If all vertices in this adjacency list are nil or discovered,
+			// pop this adjacency list from the stack and go to the next loop.
+			stack, idx = stack[:idx], idx-1
+			continue
+		}
+		v, adj = adj[i], adj[1+i:]
+		if itf.Access(v) {
+			return v
+		}
+		vAdj := itf.Adjacency(v)
+		// Following code is a simplification of the procedure:
+		//  1. Pop the old adjacency list from the stack;
+		//  2. Push the updated adjacency list (adj) to the stack if it is nonempty;
+		//  3. Push the adjacency list of the current vertex (vAdj) to the stack if it is nonempty.
+		if len(adj) > 0 {
+			stack[idx] = adj
+			if len(vAdj) > 0 {
+				stack, idx = append(stack, vAdj), idx+1
+			}
+		} else if len(vAdj) > 0 {
+			stack[idx] = vAdj
+		} else {
+			stack, idx = stack[:idx], idx-1
+		}
+	}
+	return nil
+}
+
+// DfsPath is similar to function Dfs,
+// except that it returns the path from the root of itf
+// to the goal vertex found, instead of only the goal vertex.
+//
+// It returns nil if goal is not found.
+func DfsPath(itf BasicInterface, goal interface{}) []interface{} {
+	itf.SetGoal(goal)
+	v := itf.Root()
+	if v == nil {
+		return nil
+	}
+	// It is similar to function Dfs, but the item of the stack is
+	// the list of Path instead of the adjacency list.
+	stack, idx := [][]*internal.Path{{{X: v}}}, 0
+	for idx >= 0 {
+		pl, i := stack[idx], 0
+		// Unlike in function Dfs, pl[i].X here must be non-nil.
+		for i < len(pl) && itf.Discovered(pl[i].X) {
+			i++
+		}
+		if i >= len(pl) {
+			stack, idx = stack[:idx], idx-1
+			continue
+		}
+		p, pl := pl[i], pl[1+i:]
+		if itf.Access(p.X) {
+			return p.ToList()
+		}
+		vAdj := itf.Adjacency(p.X)
+		var vAdjPathList []*internal.Path
+		if len(vAdj) > 0 {
+			vAdjPathList = make([]*internal.Path, 0, len(vAdj))
+			for _, x := range vAdj {
+				if x != nil {
+					vAdjPathList = append(vAdjPathList, &internal.Path{X: x, P: p})
+				}
+			}
+		}
+		if len(pl) > 0 {
+			stack[idx] = pl
+			if len(vAdjPathList) > 0 {
+				stack, idx = append(stack, vAdjPathList), idx+1
+			}
+		} else if len(vAdjPathList) > 0 {
+			stack[idx] = vAdjPathList
+		} else {
+			stack, idx = stack[:idx], idx-1
+		}
+	}
+	return nil
+}
+
+// Bfs finds goal in itf using breadth-first search algorithm,
+// and returns the goal vertex found.
+//
+// It returns nil if goal is not found.
+//
+// goal is only used to call the method SetGoal of itf.
+// It's OK to handle goal in your implementation of BasicInterface,
+// and set goal to an arbitrary value, such as nil.
+func Bfs(itf BasicInterface, goal interface{}) interface{} {
+	itf.SetGoal(goal)
+	v := itf.Root()
+	if v == nil {
+		return nil
+	}
+	queue := [][]interface{}{{v}}
+	for len(queue) > 0 {
+		for _, v = range queue[0] {
+			if v == nil || itf.Discovered(v) {
+				// Skip nil and discovered vertices.
+				continue
+			}
+			if itf.Access(v) {
+				return v
+			}
+			if vAdj := itf.Adjacency(v); len(vAdj) > 0 {
+				queue = append(queue, vAdj)
+			}
+		}
+		queue = queue[1:]
+	}
+	return nil
+}
+
+// BfsPath is similar to function Bfs,
+// except that it returns the path from the root of itf
+// to the goal vertex found, instead of only the goal vertex.
+//
+// It returns nil if goal is not found.
+func BfsPath(itf BasicInterface, goal interface{}) []interface{} {
+	itf.SetGoal(goal)
+	v := itf.Root()
+	if v == nil {
+		return nil
+	}
+	// It is similar to function Bfs, but the item of the queue is
+	// the list of Path instead of the adjacency list.
+	queue := [][]*internal.Path{{{X: v}}}
+	for len(queue) > 0 {
+		for _, p := range queue[0] {
+			// Unlike in function Bfs, p.X here must be non-nil.
+			if itf.Discovered(p.X) {
+				continue
+			}
+			if itf.Access(p.X) {
+				return p.ToList()
+			}
+			if vAdj := itf.Adjacency(p.X); len(vAdj) > 0 {
+				vAdjPathList := make([]*internal.Path, 0, len(vAdj))
+				for _, x := range vAdj {
+					if x != nil {
+						vAdjPathList = append(vAdjPathList, &internal.Path{X: x, P: p})
+					}
+				}
+				if len(vAdjPathList) > 0 {
+					queue = append(queue, vAdjPathList)
+				}
+			}
+		}
+		queue = queue[1:]
+	}
+	return nil
+}
+
+// Dls finds goal in itf using depth-limited depth-first search algorithm.
+//
+// limit is the maximum depth during this search.
+// The depth of the root is 0, of adjacent vertices of the root is 1, and so on.
+//
+// It returns the goal vertex found (nil if goal is not found)
+// and an indicator more to report whether there may be
+// any undiscovered vertices because of the depth limit.
+// This indicator makes sense only when the goal is not found.
+// When more is false, all the vertices must have been discovered.
+// However, when more is true, it does not guarantee that
+// there must be an undiscovered vertex,
+// because the vertex may be discovered in another search path.
+//
+// goal is only used to call the method SetGoal of itf.
+// It's OK to handle goal in your implementation of BasicInterface,
+// and set goal to an arbitrary value, such as nil.
+func Dls(itf BasicInterface, goal interface{}, limit int) (vertexFound interface{}, more bool) {
+	itf.SetGoal(goal)
+	root := itf.Root()
+	if root == nil {
+		return
+	}
+	return dls(itf, root, limit)
+}
+
+// dlsStackItem is the item in the stack used in function dls.
+// It consists of adjacency list and search depth.
+type dlsStackItem struct {
+	Adjacency []interface{}
+	Depth     int
+}
+
+// dls is the main body of function Dls,
+// without setting the goal and acquiring the root from itf.
+//
+// It requires the root to avoid redundant calls to itf.Root
+// in some functions such as Ids.
+// The client should guarantee that root is itf.Root() and root != nil.
+func dls(itf BasicInterface, root interface{}, limit int) (vertexFound interface{}, more bool) {
+	if limit < 0 {
+		return
+	}
+	// It is similar to function Dfs,
+	// except that it examines the depth before pushing a new item to the stack
+	// to guarantee that the depth does not exceed the limit.
+	stack, idx := []dlsStackItem{{Adjacency: []interface{}{root}}}, 0 // Neither idx nor len(stack) is the depth.
+	for idx >= 0 {
+		adj, i := stack[idx].Adjacency, 0
+		for i < len(adj) && (adj[i] == nil || itf.Discovered(adj[i])) {
+			i++
+		}
+		if i >= len(adj) {
+			stack, idx = stack[:idx], idx-1
+			continue
+		}
+		v, adj := adj[i], adj[1+i:]
+		if itf.Access(v) {
+			vertexFound = v
+			return
+		}
+		depth := stack[idx].Depth
+		if len(adj) > 0 {
+			stack[idx].Adjacency = adj // Just update stack[idx].Adjacency.
+		} else {
+			stack, idx = stack[:idx], idx-1
+		}
+		vAdj := itf.Adjacency(v)
+		if len(vAdj) > 0 {
+			if depth < limit {
+				// If the depth is less than the limit, push a new item.
+				stack, idx = append(stack, dlsStackItem{Adjacency: vAdj, Depth: depth + 1}), idx+1
+			} else if !more {
+				// If the depth reaches the limit,
+				// examine whether there is any more undiscovered vertex
+				// and update more.
+				for _, a := range vAdj {
+					if a != nil && !itf.Discovered(a) {
+						more = true
+						break
+					}
+				}
+			}
+		}
+	}
+	return
+}
+
+// DlsPath is similar to function Dls,
+// except that it returns the path from the root of itf
+// to the goal vertex found, instead of only the goal vertex.
+//
+// It returns nil for the path if goal is not found.
+func DlsPath(itf BasicInterface, goal interface{}, limit int) (pathFound []interface{}, more bool) {
+	itf.SetGoal(goal)
+	root := itf.Root()
+	if root == nil {
+		return
+	}
+	return dlsPath(itf, root, limit)
+}
+
+// dlsPathStackItem is the item in the stack used in function dlsPath.
+// It consists of path list and search depth.
+type dlsPathStackItem struct {
+	PathList []*internal.Path
+	Depth    int
+}
+
+// dlsPath is the main body of function DlsPath,
+// without setting the goal and acquiring the root from itf.
+//
+// It requires the root to avoid redundant calls to itf.Root
+// in some functions such as IdsPath.
+// The client should guarantee that root is itf.Root() and root != nil.
+func dlsPath(itf BasicInterface, root interface{}, limit int) (pathFound []interface{}, more bool) {
+	if limit < 0 {
+		return
+	}
+	// It is similar to function dls, but the item of the stack contains
+	// the list of Path instead of the adjacency list.
+	stack, idx := []dlsPathStackItem{{PathList: []*internal.Path{{X: root}}}}, 0 // Neither idx nor len(stack) is the depth.
+	for idx >= 0 {
+		pl, i := stack[idx].PathList, 0
+		// Unlike in function dls, pl[i].X here must be non-nil.
+		for i < len(pl) && itf.Discovered(pl[i].X) {
+			i++
+		}
+		if i >= len(pl) {
+			stack, idx = stack[:idx], idx-1
+			continue
+		}
+		p, pl := pl[i], pl[1+i:]
+		if itf.Access(p.X) {
+			pathFound = p.ToList()
+			return
+		}
+		depth := stack[idx].Depth
+		if len(pl) > 0 {
+			stack[idx].PathList = pl
+		} else {
+			stack, idx = stack[:idx], idx-1
+		}
+		vAdj := itf.Adjacency(p.X)
+		var vAdjPathList []*internal.Path
+		if len(vAdj) > 0 {
+			vAdjPathList = make([]*internal.Path, 0, len(vAdj))
+			for _, x := range vAdj {
+				if x != nil {
+					vAdjPathList = append(vAdjPathList, &internal.Path{X: x, P: p})
+				}
+			}
+		}
+		if len(vAdjPathList) > 0 {
+			if depth < limit {
+				stack, idx = append(stack, dlsPathStackItem{PathList: vAdjPathList, Depth: depth + 1}), idx+1
+			} else if !more {
+				for _, a := range vAdjPathList {
+					if !itf.Discovered(a.X) {
+						more = true
+						break
+					}
+				}
+			}
+		}
+	}
+	return
+}
+
+// IdsInterface extends interface BasicInterface for function Ids.
+//
+// It contains a new method ResetSearchState to reset
+// the search state for each iteration.
+type IdsInterface interface {
+	BasicInterface
+
+	// ResetSearchState resets the search state for the next iteration.
+	//
+	// It will be called before each iteration in function Ids,
+	// except for the first iteration.
+	//
+	// Its implementation must reset all the vertices to undiscovered,
+	// and can reset any other states associated with each iteration
+	// in this method.
+	ResetSearchState()
+}
+
+// Ids finds goal in itf using iterative deepening depth-first
+// search algorithm, and returns the goal vertex found.
+//
+// initialLimit is the depth limit used in the first iteration.
+// The depth of the root is 0, of adjacent vertices of the root is 1, and so on.
+//
+// It returns nil if goal is not found.
+//
+// goal is only used to call the method SetGoal of itf.
+// It's OK to handle goal in your implementation of IdsInterface,
+// and set goal to an arbitrary value, such as nil.
+func Ids(itf IdsInterface, goal interface{}, initialLimit int) interface{} {
+	itf.SetGoal(goal)
+	root := itf.Root()
+	if root == nil {
+		return nil
+	}
+	for limit := initialLimit; true; limit++ { // The loop ends when more is false.
+		vertexFound, more := dls(itf, root, limit)
+		if vertexFound != nil {
+			return vertexFound
+		} else if !more {
+			break
+		}
+		itf.ResetSearchState()
+	}
+	return nil
+}
+
+// IdsPath is similar to function Ids,
+// except that it returns the path from the root of itf
+// to the goal vertex found, instead of only the goal vertex.
+//
+// It returns nil if goal is not found.
+func IdsPath(itf IdsInterface, goal interface{}, initialLimit int) []interface{} {
+	itf.SetGoal(goal)
+	root := itf.Root()
+	if root == nil {
+		return nil
+	}
+	for limit := initialLimit; true; limit++ { // The loop ends when more is false.
+		pathFound, more := dlsPath(itf, root, limit)
+		if pathFound != nil {
+			return pathFound
+		} else if !more {
+			break
+		}
+		itf.ResetSearchState()
+	}
+	return nil
+}
---
+++ algorithm/search/graphv/brute_force_search_test.go
@@ -0,0 +1,434 @@
+// gogo. A Golang toolbox.
+// Copyright (C) 2019-2021 Yuan Gao
+//
+// This file is part of gogo.
+//
+// gogo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package graphv
+
+import "testing"
+
+type testGraph struct {
+	Data          [][]int
+	Goal          interface{}
+	AccessHistory []interface{}
+
+	// Index of the beginning of the access history
+	// in the most recent search iteration.
+	head int
+}
+
+func (tg *testGraph) Root() interface{} {
+	return 0
+}
+
+func (tg *testGraph) Adjacency(vertex interface{}) []interface{} {
+	list := tg.Data[vertex.(int)]
+	if len(list) == 0 {
+		return nil
+	}
+	adj := make([]interface{}, len(list)+4)
+	// Add 4 nil vertices for testing.
+	switch len(list) {
+	case 1:
+		// adj: {nil, nil, nil, list[0], nil}
+		adj[4] = list[0]
+	case 2:
+		// adj: {nil, list[0], nil, nil, list[1], nil}
+		adj[1] = list[0]
+		adj[5] = list[1]
+	default:
+		// adj: {list[0], nil, list[1], ..., list[n-2], nil, nil, nil, list[n-1]}
+		// where n = len(list)
+		adj[0] = list[0]
+		for i := 1; i < len(list)-1; i++ {
+			adj[i+1] = list[i]
+		}
+		adj[len(adj)-1] = list[len(list)-1]
+	}
+	return adj
+}
+
+// SetGoal sets the search goal as well as resets the access history.
+func (tg *testGraph) SetGoal(goal interface{}) {
+	tg.Goal = goal
+	tg.AccessHistory = tg.AccessHistory[:0] // Reuse the underlying array.
+	tg.head = 0
+}
+
+func (tg *testGraph) Access(vertex interface{}) (found bool) {
+	tg.AccessHistory = append(tg.AccessHistory, vertex)
+	if tg.Goal == nil {
+		return false
+	}
+	return vertex == tg.Goal
+}
+
+// Discovered reports whether the specified vertex
+// has been examined by the method Access
+// via checking the access history.
+//
+// It may be time-consuming,
+// but it doesn't matter because the test graph is very small.
+func (tg *testGraph) Discovered(vertex interface{}) bool {
+	for i := tg.head; i < len(tg.AccessHistory); i++ {
+		if tg.AccessHistory[i] == vertex {
+			return true
+		}
+	}
+	return false
+}
+
+func (tg *testGraph) ResetSearchState() {
+	tg.head = len(tg.AccessHistory)
+}
+
+// testUndirectedGraphData represents an undirected graph as follows:
+//      0
+//     /|\
+//    1 2 3
+//   /| | |
+//  4 5 6 |
+//     \_/
+//
+// Assuming that the search starts at the vertex 0,
+// and the left edges are chosen before the right edges,
+// the expected orderings are as follows:
+//
+// Expected DFS ordering:
+//  0, 1, 4, 5, 3, 2, 6
+// Expected BFS ordering:
+//  0, 1, 2, 3, 4, 5, 6
+var testUndirectedGraphData = [][]int{
+	{1, 2, 3}, // vertex 0
+	{0, 4, 5}, // vertex 1
+	{0, 6},    // vertex 2
+	{0, 5},    // vertex 3
+	{1},       // vertex 4
+	{1, 3},    // vertex 5
+	{2},       // vertex 6
+}
+
+// testUndirectedGraphDataOrderingMap is a mapping from algorithm short names
+// to the expected vertex access orderings of testUndirectedGraphData.
+//
+// Valid keys:
+//  dfs
+//  bfs
+//  dls-0
+//  dls-1
+//  dls-2
+//  dls-3
+//  ids
+var testUndirectedGraphDataOrderingMap = map[string][]int{
+	"dfs":   {0, 1, 4, 5, 3, 2, 6},
+	"bfs":   {0, 1, 2, 3, 4, 5, 6},
+	"dls-0": {0},
+	"dls-1": {0, 1, 2, 3},
+	"dls-2": {0, 1, 4, 5, 2, 6, 3},
+	"dls-3": nil, // It is the same as dfs and will be set in function init.
+	"ids":   {0, 0, 1, 2, 3, 0, 1, 4, 5, 2, 6, 3, 0, 1, 4, 5, 3, 2, 6},
+}
+
+// testUndirectedGraphDataVertexPathMap is a mapping from algorithm short names
+// to lists of paths from the root to each vertex of testUndirectedGraphData.
+//
+// Valid keys:
+//  dfs
+//  bfs
+//  dls-0
+//  dls-1
+//  dls-2
+//  dls-3
+//  ids
+var testUndirectedGraphDataVertexPathMap = map[string][][]int{
+	"dfs": {
+		{0},
+		{0, 1},
+		{0, 2},
+		{0, 1, 5, 3},
+		{0, 1, 4},
+		{0, 1, 5},
+		{0, 2, 6},
+	},
+	"bfs": {
+		{0},
+		{0, 1},
+		{0, 2},
+		{0, 3},
+		{0, 1, 4},
+		{0, 1, 5},
+		{0, 2, 6},
+	},
+	"dls-0": {
+		{0},
+		nil, nil, nil, nil, nil, nil, // nil means the corresponding vertex has not been accessed.
+	},
+	"dls-1": {
+		{0},
+		{0, 1},
+		{0, 2},
+		{0, 3},
+		nil, nil, nil, // nil means the corresponding vertex has not been accessed.
+	},
+	"dls-2": nil, // It is the same as bfs and will be set in function init.
+	"dls-3": nil, // It is the same as dfs and will be set in function init.
+	"ids":   nil, // It is the same as bfs and will be set in function init.
+}
+
+func init() {
+	testUndirectedGraphDataOrderingMap["dls-3"] = testUndirectedGraphDataOrderingMap["dfs"]
+
+	testUndirectedGraphDataVertexPathMap["dls-2"] = testUndirectedGraphDataVertexPathMap["bfs"]
+	testUndirectedGraphDataVertexPathMap["dls-3"] = testUndirectedGraphDataVertexPathMap["dfs"]
+	testUndirectedGraphDataVertexPathMap["ids"] = testUndirectedGraphDataVertexPathMap["bfs"]
+}
+
+func TestDfs(t *testing.T) {
+	testBruteForceSearch(t, "Dfs")
+}
+
+func TestDfsPath(t *testing.T) {
+	testBruteForceSearchPath(t, "DfsPath")
+}
+
+func TestBfs(t *testing.T) {
+	testBruteForceSearch(t, "Bfs")
+}
+
+func TestBfsPath(t *testing.T) {
+	testBruteForceSearchPath(t, "BfsPath")
+}
+
+func TestDls(t *testing.T) {
+	testBruteForceSearch(t, "Dls-0")
+	testBruteForceSearch(t, "Dls-1")
+	testBruteForceSearch(t, "Dls-2")
+	testBruteForceSearch(t, "Dls-3")
+}
+
+func TestDlsPath(t *testing.T) {
+	testBruteForceSearchPath(t, "DlsPath-0")
+	testBruteForceSearchPath(t, "DlsPath-1")
+	testBruteForceSearchPath(t, "DlsPath-2")
+	testBruteForceSearchPath(t, "DlsPath-3")
+}
+
+func TestIds(t *testing.T) {
+	testBruteForceSearch(t, "Ids")
+}
+
+func TestIdsPath(t *testing.T) {
+	testBruteForceSearchPath(t, "IdsPath")
+}
+
+func testBruteForceSearch(t *testing.T, name string) {
+	var f func(itf BasicInterface, goal interface{}) interface{}
+	var ordering []int
+	switch name {
+	case "Dfs":
+		f = Dfs
+		ordering = testUndirectedGraphDataOrderingMap["dfs"]
+	case "Bfs":
+		f = Bfs
+		ordering = testUndirectedGraphDataOrderingMap["bfs"]
+	case "Dls-0":
+		f = func(itf BasicInterface, goal interface{}) interface{} {
+			v, more := Dls(itf, goal, 0)
+			if v == nil && !more {
+				t.Error("Dls-0 - more is false but there are undiscovered vertices.")
+			}
+			return v
+		}
+		ordering = testUndirectedGraphDataOrderingMap["dls-0"]
+	case "Dls-1":
+		f = func(itf BasicInterface, goal interface{}) interface{} {
+			v, more := Dls(itf, goal, 1)
+			if v == nil && !more {
+				t.Error("Dls-1 - more is false but there are undiscovered vertices.")
+			}
+			return v
+		}
+		ordering = testUndirectedGraphDataOrderingMap["dls-1"]
+	case "Dls-2":
+		f = func(itf BasicInterface, goal interface{}) interface{} {
+			v, _ := Dls(itf, goal, 2)
+			// Both true and false are acceptable for the second return value.
+			return v
+		}
+		ordering = testUndirectedGraphDataOrderingMap["dls-2"]
+	case "Dls-3":
+		f = func(itf BasicInterface, goal interface{}) interface{} {
+			v, _ := Dls(itf, goal, 3)
+			// Both true and false are acceptable for the second return value.
+			return v
+		}
+		ordering = testUndirectedGraphDataOrderingMap["dls-3"]
+	case "Ids":
+		f = func(itf BasicInterface, goal interface{}) interface{} {
+			return Ids(itf.(IdsInterface), goal, 0)
+		}
+		ordering = testUndirectedGraphDataOrderingMap["ids"]
+	default:
+		t.Error("Unacceptable name:", name)
+		return
+	}
+
+	tg := &testGraph{Data: testUndirectedGraphData}
+	tested := make([]bool, len(testUndirectedGraphData))
+	for i, v := range ordering {
+		if tested[v] {
+			continue
+		}
+		tested[v] = true
+		r := f(tg, v)
+		if r != v {
+			t.Errorf("%s returns %v != %v.", name, r, v)
+		}
+		testCheckAccessHistory(t, name, tg, ordering[:1+i])
+	}
+	// Non-existent nodes:
+	for _, goal := range []interface{}{nil, -1, len(testUndirectedGraphData), 1.2} {
+		r := f(tg, goal)
+		if r != nil {
+			t.Errorf("%s returns %v != nil.", name, r)
+		}
+		testCheckAccessHistory(t, name, tg, ordering)
+	}
+}
+
+func testBruteForceSearchPath(t *testing.T, name string) {
+	var f func(itf BasicInterface, goal interface{}) []interface{}
+	var ordering []int
+	switch name {
+	case "DfsPath":
+		f = DfsPath
+		ordering = testUndirectedGraphDataOrderingMap["dfs"]
+	case "BfsPath":
+		f = BfsPath
+		ordering = testUndirectedGraphDataOrderingMap["bfs"]
+	case "DlsPath-0":
+		f = func(itf BasicInterface, goal interface{}) []interface{} {
+			p, more := DlsPath(itf, goal, 0)
+			if p == nil && !more {
+				t.Error("DlsPath-0 - more is false but there are undiscovered vertices.")
+			}
+			return p
+		}
+		ordering = testUndirectedGraphDataOrderingMap["dls-0"]
+	case "DlsPath-1":
+		f = func(itf BasicInterface, goal interface{}) []interface{} {
+			p, more := DlsPath(itf, goal, 1)
+			if p == nil && !more {
+				t.Error("DlsPath-1 - more is false but there are undiscovered vertices.")
+			}
+			return p
+		}
+		ordering = testUndirectedGraphDataOrderingMap["dls-1"]
+	case "DlsPath-2":
+		f = func(itf BasicInterface, goal interface{}) []interface{} {
+			p, _ := DlsPath(itf, goal, 2)
+			// Both true and false are acceptable for the second return value.
+			return p
+		}
+		ordering = testUndirectedGraphDataOrderingMap["dls-2"]
+	case "DlsPath-3":
+		f = func(itf BasicInterface, goal interface{}) []interface{} {
+			p, _ := DlsPath(itf, goal, 3)
+			// Both true and false are acceptable for the second return value.
+			return p
+		}
+		ordering = testUndirectedGraphDataOrderingMap["dls-3"]
+	case "IdsPath":
+		f = func(itf BasicInterface, goal interface{}) []interface{} {
+			return IdsPath(itf.(IdsInterface), goal, 0)
+		}
+		ordering = testUndirectedGraphDataOrderingMap["ids"]
+	default:
+		t.Error("Unacceptable name:", name)
+		return
+	}
+
+	tg := &testGraph{Data: testUndirectedGraphData}
+	tested := make([]bool, len(testUndirectedGraphData))
+	for i, v := range ordering {
+		if tested[v] {
+			continue
+		}
+		tested[v] = true
+		list := f(tg, v)
+		testCheckPath(t, name, v, list)
+		testCheckAccessHistory(t, name, tg, ordering[:1+i])
+	}
+	// Non-existent nodes:
+	for _, goal := range []interface{}{nil, -1, len(testUndirectedGraphData), 1.2} {
+		list := f(tg, goal)
+		testCheckPath(t, name, goal, list)
+		testCheckAccessHistory(t, name, tg, ordering)
+	}
+}
+
+func testCheckAccessHistory(t *testing.T, name string, tg *testGraph, wanted []int) {
+	if len(tg.AccessHistory) != len(wanted) {
+		t.Errorf("%s - Access history: %v\nwanted: %v", name, tg.AccessHistory, wanted)
+		return
+	}
+	for i := range wanted {
+		if tg.AccessHistory[i] != wanted[i] {
+			t.Errorf("%s - Access history: %v\nwanted: %v", name, tg.AccessHistory, wanted)
+			return
+		}
+	}
+}
+
+func testCheckPath(t *testing.T, name string, vertex interface{}, pathList []interface{}) {
+	var p []int
+	var list [][]int
+	switch name {
+	case "DfsPath":
+		list = testUndirectedGraphDataVertexPathMap["dfs"]
+	case "BfsPath":
+		list = testUndirectedGraphDataVertexPathMap["bfs"]
+	case "DlsPath-0":
+		list = testUndirectedGraphDataVertexPathMap["dls-0"]
+	case "DlsPath-1":
+		list = testUndirectedGraphDataVertexPathMap["dls-1"]
+	case "DlsPath-2":
+		list = testUndirectedGraphDataVertexPathMap["dls-2"]
+	case "DlsPath-3":
+		list = testUndirectedGraphDataVertexPathMap["dls-3"]
+	case "IdsPath":
+		list = testUndirectedGraphDataVertexPathMap["ids"]
+	default:
+		// This should never happen, but will act as a safeguard for later,
+		// as a default value doesn't make sense here.
+		t.Error("Unacceptable name:", name)
+		return
+	}
+	idx, ok := vertex.(int)
+	if ok && idx >= 0 && idx < len(list) {
+		p = list[idx]
+	}
+	if len(pathList) != len(p) {
+		t.Errorf("%s - Path of %v: %v\nwanted: %v", name, vertex, pathList, p)
+		return
+	}
+	for i := range p {
+		if pathList[i] != p[i] {
+			t.Errorf("%s - Path of %v: %v\nwanted: %v", name, vertex, pathList, p)
+			return
+		}
+	}
+}
