3fe885a411d8181cf5084c5ed576303291306614
Yuan Gao <donyoridoyodoyo@outlook.com>
2022-04-03 03:49:16 +0800

    Restore an accidentally deleted document.

--- copyright/doc.go
+++ copyright/doc.go
@@ -16,4 +16,5 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+// Package copyright provides functions about copyright and licenses.
 package copyright
