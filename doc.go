76444609fc81f14f8641fef3f88e10ef04ec8d1b
Yuan Gao <donyoridoyodoyo@outlook.com>
2022-04-06 00:22:12 +0800

    Add document.

---
+++ doc.go
@@ -0,0 +1,22 @@
+// gogo.  A Go (Golang) toolbox.
+// Copyright (C) 2019-2022  Yuan Gao
+//
+// This file is part of gogo.
+//
+// gogo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// Package gogo provides tools for programming with Go (Golang).
+//
+// See its subpackages for details on its function.
+package gogo
